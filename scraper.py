77da375754a653cb04b0555c6179425c78da51e3
bearachute <sjourand@gmail.com>
2017-07-03 22:24:05 -0500

    still working on xpath :pizza: thumbsdown: :poop:

--- scraper.py
+++ scraper.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import scrapy
 
+
 class ChonSpider(scrapy.Spider):
-	name = "Chon_scrape"
-	start_urls = ['http://4chan.org/co/']
 
-def parse(self, response):
-	SET_SELECTOR = 'img'
-	for chon in response.css(SET_SELECTOR):
+    name = 'Chon_scrape'
+    start_urls = ['http://boards.4chan.org/co/']
 
-		NAME_SELECTOR = 'span.dateTime'
-		yield {
-			'date': chon.css(NAME_SELECTOR).extract_first(),
-		}
+    def parse(self, response):
+        SET_SELECTOR = 'span.subject'
+        for chons in response.css(SET_SELECTOR):
+
+            NAME_SELECTOR = 'post op'
+        yield {'text': chons.css(NAME_SELECTOR).extract_first()}
